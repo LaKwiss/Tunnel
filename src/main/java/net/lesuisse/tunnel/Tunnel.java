75bdcadfe0cdc096ff0da1f14271cc3ca6ee9aa8
LaKwiss <y.baelli05@gmail.com>
2023-05-11 20:21:43 +0200

    Add to ingredients tab + remove zircon + add black opal
    New Textures black_opal.png

--- src/main/java/net/lesuisse/tunnel/Tunnel.java
+++ src/main/java/net/lesuisse/tunnel/Tunnel.java
@@ -1,9 +1,11 @@
 package net.lesuisse.tunnel;
 
 import com.mojang.logging.LogUtils;
-import net.lesuisse.tunnel.items.ModItems;
+import net.lesuisse.tunnel.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -25,13 +27,21 @@ public class Tunnel
         ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if(event.getTab() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BLACK_OPAL);
+        }
+    }
+
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
